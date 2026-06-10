df12aa5f85812af44ce303bb87e8f62fcaf8f030
Evangelina Vega <evangelina1001@hotmail.com>
2026-06-09 23:58:38 -0700

    removed: fullscreen carousel

--- caro-portfolio/components/Carousel/photoCarousel.tsx
+++ caro-portfolio/components/Carousel/photoCarousel.tsx
@@ -1,29 +1,43 @@
 "use client";
 
 import { ImageType } from "@/types/ImageType";
+import Image from "next/image";
 import { useState } from "react";
-import CollectionCarousel from "./collectionCarousel";
-import FullscreenCarousel from "./fullscreenCarousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 
 export default function PhotoCarousel({ images }: { images: ImageType[] }) {
   const [open, setOpen] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
-      <CollectionCarousel
-        images={images}
-        onImageClick={(index) => {
-          setOpen(true);
-          setSelectedIndex(index);
-        }}
-      />
-      <FullscreenCarousel
-        images={images}
-        open={open}
-        onOpenChange={setOpen}
-        selectedIndex={selectedIndex}
-      />
+      <Carousel className="w-full" opts={{ loop: true }}>
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className="w-full">
+                <Image
+                  src={image.url}
+                  width={image.width}
+                  height={image.height}
+                  alt={`${index + 1}`}
+                  className="object-cover w-full h-auto cursor-zoom-in"
+                  onClick={() => setOpen(open)}
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="flex items-center justify-center gap-2.5">
+          <CarouselPrevious className="static" />
+          <CarouselNext className="static" />
+        </div>
+      </Carousel>
     </>
   );
 }
