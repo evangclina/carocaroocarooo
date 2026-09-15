8cfd529e06fbebae0baf9601d344b607cff38b69
Evangelina <evangelina1001@hotmail.com>
2026-09-14 19:19:14 -0700

    fixed data structure

--- caro-portfolio/i18n/request.ts
+++ caro-portfolio/i18n/request.ts
@@ -1,3 +1,17 @@
+// import { getRequestConfig } from "next-intl/server";
+// import { routing } from "./routing";
+
+// export default getRequestConfig(async ({ requestLocale }) => {
+//   let locale = await requestLocale;
+
+//   if (!locale || !routing.locales.includes(locale as "es" | "en")) {
+//     locale = routing.defaultLocale;
+//   }
+//   return {
+//     locale,
+//     message: (await import(`../messages/${locale}.json`)).default,
+//   };
+// });
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
@@ -7,8 +21,9 @@ export default getRequestConfig(async ({ requestLocale }) => {
   if (!locale || !routing.locales.includes(locale as "es" | "en")) {
     locale = routing.defaultLocale;
   }
+
   return {
     locale,
-    message: (await import(`../messages/${locale}.json`)).default,
+    messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
